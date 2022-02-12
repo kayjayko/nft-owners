cf71d11395525db3c07929c05edd111f47aee0af
Katy Jeremko <katyjeremko@macbook-pro-2.lan>
2022-02-12 12:39:05 -0700

    Creates script.

---
+++ readme.md
@@ -0,0 +1,9 @@
+# NFT Owners
+
+Grabs NFT address owners and a count for number of NFTs held by contract address.
+
+## Set-up
+
+- Create a Moralis account (or use public account in example) and set api config in `scripts/fetch-owners.js`
+- `npm install`
+- `npm run fetchOwners`
---
+++ scripts/fetch-owners.js
@@ -0,0 +1,36 @@
+const Moralis = require('moralis/node');
+
+// Moralis server
+Moralis.start({
+  serverUrl: 'https://dxzgmomfm2d8.usemoralis.com:2053/server',
+  appID: 'Nfsvr5ggrpMgUeNNR9JdOcYMMLTibggVOy0lxGG3'
+})
+
+// This does not paginate, since all these projects are under pagination limits
+// To paginate, pass `resp.cursor` to the next call
+async function fetchByContract(address) {
+  const options = { address, chain: "eth" };
+  const resp = await Moralis.Web3API.token.getNFTOwners(options);
+  return resp.result
+}
+
+async function main() {
+  const acab = await fetchByContract('0xd455550af4a7558b0beec5eaec2cc415c4ff4039')
+  const lgnd = await fetchByContract('0x807b7601aa00Ab306254e19c17aADC5c361354A4')
+  const punish = await fetchByContract('0xe00D98151B43fA6f2B0888952A392CfC9Cf6Fc24')
+  const nfts = [...acab, ...lgnd, ...punish]
+
+  // Mapping owner counts
+  const owners = {}
+  nfts.forEach(r => {
+    if (!owners[r.owner_of]) {
+      owners[r.owner_of] = 0
+    }
+    owners[r.owner_of]++
+  })
+
+  console.log(owners)
+  
+}
+
+main()
